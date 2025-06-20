5eeb7e59fecd715abfd31fbc22c2840e1c6740fa
Andreea Racovita <andreearacovitagit@gmail.com>
2025-06-20 11:26:09 +0300

    [Hosting Calendar] Draft property selection panel

--- src/components/hosting/HostingCalendarSettings.tsx
+++ src/components/hosting/HostingCalendarSettings.tsx
@@ -1,11 +1,44 @@
+import { useState } from "react";
+import * as Icon from "react-bootstrap-icons";
+import { useSearchParams } from "react-router";
+
+enum State {
+	Selection,
+	Settings
+};
+
 export default function HostingCalendarSettings() {
+	const [searchParams, setSearchParams] = useSearchParams();
+	const [state, setState] = useState(State.Selection);
+
+	function handlePropertyClick() {
+		searchParams.set("id", 1); // Prop ID
+		setSearchParams(searchParams);
+		setState(State.Settings);
+	}
+
+	function handleBackClick() {
+		searchParams.delete("id");
+		setSearchParams(searchParams);
+		setState(State.Selection);
+	}
 	
 	return (
 		<div className="border-section">
-			<div>If more properties:</div>
-			<div className="section-heading">Choose a property</div>
-			<div>List of managed properties</div>
-			<div className="section-heading">Settings</div>
+			{
+				state === State.Selection &&
+				<>
+					<div className="section-heading">Manage property</div>
+					<div>[Managed properties list here]</div>
+				</>
+			}
+			{
+				state === State.Settings &&
+				<>
+					<div><Icon.ChevronLeft /> Choose another property</div>
+					<div className="section-heading">Prop title</div>
+				</>
+			}
 		</div>
 	);
 }
