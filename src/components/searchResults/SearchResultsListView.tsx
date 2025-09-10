a3a9ab2d47a6267927cd79ed213571344afa23b6
Andreea Racovita <andreearacovitagit@gmail.com>
2025-09-10 16:07:12 +0300

    [Search results] Fix list view for mobile layout

--- src/components/searchResults/SearchResultsListView.tsx
+++ src/components/searchResults/SearchResultsListView.tsx
@@ -1,4 +1,4 @@
-import { Map } from "react-bootstrap-icons";
+import { Filter as FilterIcon, Map } from "react-bootstrap-icons";
 import { useSearchParams } from "react-router";
 import countries from "react-select-country-list";
 
@@ -47,32 +47,89 @@ export default function SearchResultsListView({
 	return (
 		<div className="container section-container">
 			<Search />
-			<div className="section-container d-flex">
-				<div className="search-page-fixed-width-left">
-					<div className="position-relative">
-						<MapView
-							id="mini-map"
-							height={200}
-							center={geo.coordinate}
-							boundingbox={geo.boundingbox}
-							zoom={14}
-							points={[]}
-							isEditable={false}
-							updatePinPosition={undefined}
-							updateIdsMap={undefined}
-							handleHighlightItem={undefined}
-							shouldShowText={undefined}
-							priceMap={undefined}
-						/>
-						<span
-							className="btn-pill position-absolute top-50 start-50 translate-middle"
-							onClick={() => setIsFullscreenMap(true)}
-						>
-							<span className="me-1">Show map</span>
-							<Map color="white" />
-						</span>
+			<div className="d-none d-md-block">
+				{/*Desktop*/}
+				<div className="section-container d-flex">
+					<div className="search-page-fixed-width-left">
+						<div className="position-relative">
+							<MapView
+								id="mini-map-desktop"
+								height={200}
+								center={geo.coordinate}
+								boundingbox={geo.boundingbox}
+								zoom={14}
+								points={[]}
+								isEditable={false}
+								updatePinPosition={undefined}
+								updateIdsMap={undefined}
+								handleHighlightItem={undefined}
+								shouldShowText={undefined}
+								priceMap={undefined}
+							/>
+							<span
+								className="btn-pill position-absolute top-50 start-50 translate-middle"
+								onClick={() => setIsFullscreenMap(true)}
+							>
+								<span className="me-1">Show map</span>
+								<Map color="white" />
+							</span>
+						</div>
+						<div className="section-container">
+							<Filter
+								city={city}
+								lowestPrice={priceRange[0]}
+								highestPrice={priceRange[1]}
+							/>
+						</div>
 					</div>
-					<div className="section-container">
+					<div className="ms-2">
+						<h1 className="page-heading">{getLocationString()}: {countString} found</h1>
+						<div className="mt-10">
+							<ListView
+								listItemType={ListItemType.Property}
+								items={items}
+								checkIn={checkInParam}
+								checkOut={checkOutParam}
+								guests={guests}
+								nightsCount={nightsCount}
+								cols={3}
+								isCompact={false}
+								setNeedsRefresh={undefined}
+							/>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div className="d-block d-md-none">
+				{/*Mobile*/}
+				<div className="position-relative">
+					<MapView
+						id="mini-map-mobile"
+						height={150}
+						center={geo.coordinate}
+						boundingbox={geo.boundingbox}
+						zoom={14}
+						points={[]}
+						isEditable={false}
+						updatePinPosition={undefined}
+						updateIdsMap={undefined}
+						handleHighlightItem={undefined}
+						shouldShowText={undefined}
+						priceMap={undefined}
+					/>
+					<span
+						className="btn-pill position-absolute top-50 start-50 translate-middle"
+						onClick={() => setIsFullscreenMap(true)}
+					>
+						<span className="me-1">Show map</span>
+						<Map color="white" />
+					</span>
+				</div>
+				<div className="dropdown mt-2">
+					<div id="dropdown-button" role="button" data-bs-toggle="dropdown" data-bs-auto-close="false">
+						<span className="btn-pill-outline">Filters <FilterIcon size={24} /></span>
+					</div>
+					<div className="dropdown-menu dropdown-menu-start">
 						<Filter
 							city={city}
 							lowestPrice={priceRange[0]}
@@ -80,23 +137,24 @@ export default function SearchResultsListView({
 						/>
 					</div>
 				</div>
-				<div className="ms-2">
+				<div className="m-2">
 					<h1 className="page-heading">{getLocationString()}: {countString} found</h1>
 					<div className="mt-10">
 						<ListView
-							listItemType={ListItemType.Property}
+							listItemType={ListItemType.SmallProperty}
 							items={items}
 							checkIn={checkInParam}
 							checkOut={checkOutParam}
 							guests={guests}
 							nightsCount={nightsCount}
-							cols={3}
+							cols={1}
 							isCompact={false}
 							setNeedsRefresh={undefined}
 						/>
 					</div>
 				</div>
 			</div>
+			
 		</div>
 	)
 }
